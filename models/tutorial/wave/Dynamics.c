716b03cdd635525dd8a6b024b01418b7c7727357
dlightbody <40252256+dlightbody@users.noreply.github.com>
2018-07-19 14:15:49 +1000

    Update Dynamics.c

--- models/tutorial/wave/Dynamics.c
+++ models/tutorial/wave/Dynamics.c
@@ -1,1 +1,22 @@
+CudaDeviceFunction float2 Color() {
+  float2 ret;
+  ret.x = 0;
+  ret.y = 1;
+  return ret;
+}
+
+CudaDeviceFunction void Init() { }
+
+CudaDeviceFunction void Run() { }
+
+CudaDeviceFunction void Init() {
+  u = ;
+  v = 0;
+}
+
+CudaDeviceFunction void Run() {
+  u = u(0,0);
+  v = v(0,0);
+}
+
 
