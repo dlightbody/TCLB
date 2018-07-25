a1f53118dd0cf24a15d7726c8af40c1acc4c7d50
dlightbody <40252256+dlightbody@users.noreply.github.com>
2018-07-26 08:09:23 +1000

    Update Dynamics.c

--- models/tutorial/wave/Dynamics.c
+++ models/tutorial/wave/Dynamics.c
@@ -12,7 +12,8 @@ CudaDeviceFunction void Init() {
 
 CudaDeviceFunction void Run() {
   real_t lap_u = u(-1,0) + u(1,0) + u(0,-1) + u(0,1) - 4*u(0,0);
-  real_t a = Speed * Speed * lap_u;
+  real_t lap_v = v(-1,0) + v(1,0) + v(0,-1) + v(0,1) - 4*v(0,0);
+  real_t a = Speed * Speed * lap_u + Viscosity * lap_v;
   v = v(0,0) + a;
   u = u(0,0) + v;
 }
